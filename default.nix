6667a98b91409cfbea3264bb5338f255f1f63c19
Blake Smith <blakesmith0@gmail.com>
2023-05-21 14:39:04 -0500

    Wire in the gnome desktop files as well

--- default.nix
+++ default.nix
@@ -7,12 +7,12 @@
 with rustPlatform;
 
 buildRustPackage rec {
-  pname = "otptrap";
+  pname = "otptray";
   version = "0.1.0";
   src = builtins.filterSource
     (path: type: type != "directory" || baseNameOf path != "target")
     ./.;
-  cargoSha256 = "RwM+8txnToOuniTEagXjz0BtMvOjjyPbIuxBP7ic7Io=";
+  cargoSha256 = "s5W5l+C1cuGTO9nHBkWPe0IfWIKO1+nn+HUp6ua61Wg=";
   nativeBuildInputs = [
     pkgs.clang
     pkgs.pkg-config
@@ -28,6 +28,19 @@ buildRustPackage rec {
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion pkgs.clang}/include";
 
+
+  # Update all gnome desktop application binary references
+  postPatch = ''
+  substituteInPlace share/applications/otptray.desktop \
+    --replace /usr $out
+  '';
+
+  installPhase = ''
+  mkdir -p $out/bin $out/share
+  cp -rv share $out/
+  find target/ -name "otptray" -exec cp '{}' $out/bin \;
+  '';
+  
   meta = with lib; {
     description = "OTPTray 2FA / OTP helper appindicator tray application";
     homepage = "https://blakesmith.me";
